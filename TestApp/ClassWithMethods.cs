4e461654e8d82817c0dcfc15eca65144f43880f7
stephen whitcomb <stephen whitcomb>
2015-12-21 16:55:58 -0500

    initial commit

---
+++ TestApp/ClassWithMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    public class ClassWithMethods
+    {
+        public RelayCommand buttonCommand { get; set; }
+        public int counter;
+        public ClassWithMethods()
+        {
+            buttonCommand = new RelayCommand(printToConsole);
+            counter = 1;
+        }
+
+        public void printToConsole(object parameter)
+        {
+            Console.WriteLine("You clicked me " + counter++ + " times.");
+        }
+    }
+}
